61b91ce7dd7e1691b16cb3c19c64531c33a4bb95
cipher <dilsurbet@hmail.com>
2024-06-05 17:19:14 +0500

    1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const KEY = "d1530aa5b1b3af93250ab35f97790768";
+const base = "https://api.openweathermap.org/data/2.5/weather";
 const changeLocation = document.getElementById("change-location");
 const card = document.getElementById("card");
 const details = document.getElementById("details");
@@ -6,23 +8,33 @@ const overlay = document.getElementById("overlay");
 
 changeLocation.city.focus();
 
-const updateUI = (weather) => {
-  console.log(weather.weather[0].icon);
-  console.log(weather);
-  card.classList.remove("d-none");
-  details.innerHTML = `
-            <h5 class="mb-3">${weather.name}, ${weather.sys.country}</h5>
-            <p class="mb-3">${weather.weather[0].main}</p>
-        <div class="display-4 mb-3">
-            <span>${Math.round(weather.main.temp)}</span>
-            <span>&deg;C</span>
-        </div>
-    `;
-  weatherIcon.src = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
-};
-
 const getWeather = async (city) => {
-  const data = await getData(city);
+  const data = await fetch(
+    base + "?q=" + city + "&appid=" + KEY + "&units=metric"
+  )
+    .then((data) => {
+      overlay.classList.remove("d-none");
+      return data.json();
+    })
+    .then((resultData) => {
+      overlay.classList.add("d-none");
+      card.classList.remove("d-none");
+      details.innerHTML = `
+                <h5 class="mb-3">${resultData.name}, ${
+        resultData.sys.country
+      }</h5>
+                <p class="mb-3">${resultData.weather[0].main}</p>
+            <div class="display-4 mb-3">
+                <span>${Math.round(resultData.main.temp)}</span>
+                <span>&deg;C</span>
+            </div>
+        `;
+      weatherIcon.src = `https://openweathermap.org/img/wn/${resultData.weather[0].icon}@2x.png`;
+      return resultData;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   return data;
 };
 
@@ -30,5 +42,5 @@ changeLocation.addEventListener("submit", (e) => {
   e.preventDefault();
   const cityName = changeLocation.city.value.trim();
   changeLocation.reset();
-  getWeather(cityName).then((data) => updateUI(data));
+  getWeather(cityName);
 });
